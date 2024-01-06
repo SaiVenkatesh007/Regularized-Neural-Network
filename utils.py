b9a5808811245e9d5c3aa56403e560f67f0014d8
SaiVenkatesh007 <saivenki65@gmail.com>
2024-01-06 20:00:55 +0530

    Added Linear Regression Model
    Add the class for Linear Model in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+
 def gen_data(m, seed=1, scale=0.7):
     c=0
     x_train = np.linspace(0,49,m)
@@ -8,3 +12,27 @@ def gen_data(m, seed=1, scale=0.7):
     y_train = y_ideal + scale * y_ideal * (np.random.sample((m,))-0.5)
     x_ideal = x_train
     return x_train, y_train, x_ideal, y_ideal
+
+class LinModel:
+    def __init__(self, degree, regularization=False, lambda_=0):
+        if regularization:
+            self.linear_model = Ridge(alpha=lambda_)
+        else:
+            self.linear_model = LinearRegression()
+        self.scaler = StandardScaler()
+        self.poly = PolynomialFeatures(degree=degree, include_bias=False)
+    
+    def fit(self, X_train, y_train):
+        X_train_mapped = self.poly.fit_transform(X_train.reshape(-1,1))
+        X_train_mapped_scaled = self.scaler.fit_transform(X_train_mapped)
+        self.linear_model.fit(X_train_mapped_scaled, y_train)
+    
+    def predict(self, X):
+        X_mapped = self.poly.transform(X.reshape(-1,1))
+        X_mapped_scaled = self.scaler.transform(X_mapped)
+        ypred = self.linear_model.predict(X_mapped_scaled)
+        return(ypred)
+    
+    def mse(self, y, ypred):
+        err = mean_squared_error(y, ypred)/2
+        return(err)
